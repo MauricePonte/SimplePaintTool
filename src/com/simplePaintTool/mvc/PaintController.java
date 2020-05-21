28feab0b96f9efe71cc153b5e2dbc275ed327540
Maurice <maurice.leon.ponte@gmail.com>
2020-05-21 15:47:28 +0200

    Drawing Shapes

--- src/com/simplePaintTool/mvc/PaintController.java
+++ src/com/simplePaintTool/mvc/PaintController.java
@@ -1,8 +1,16 @@
 package com.simplePaintTool.mvc;
 
 import com.simplePaintTool.PaintingFrame;
+import com.simplePaintTool.commands.AddShapeCommand;
 import com.simplePaintTool.commands.Command;
+import com.simplePaintTool.shapes.Rectangle;
+import com.simplePaintTool.shapes.Shape;
 
+import java.awt.*;
+import java.awt.event.MouseAdapter;
+import java.awt.event.MouseEvent;
+
+import java.util.ArrayList;
 import java.util.Stack;
 
 public class PaintController {
@@ -19,4 +27,15 @@ public class PaintController {
         undoCommands = new Stack<>();
     }
 
+    private void executeCommand(Command c){
+        c.execute();
+        commands.push(c);
+
+        frame.getView().repaint();
+    }
+
+    public void btnAddRectangleClicked(Point[] p) {
+        Shape s = new Rectangle(p[0],p[1]);
+        executeCommand(new AddShapeCommand(s,model));
+    }
 }
